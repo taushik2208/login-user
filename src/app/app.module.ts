3e745093a0a4a9f6450966674697ca38a8bda23f
Taushik Das <taushikedu@gmail.com>
2020-12-11 23:07:16 +0530

    Added login, signup, main & main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    LoginComponent,
+    SignupComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
